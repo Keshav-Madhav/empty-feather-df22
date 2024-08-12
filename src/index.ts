e2f733ad80a953b5f64257bc91061cdd0055a428
Keshav Madhav <keshav2552003@gmail.com>
2024-08-12 15:35:38 +0530

    Added chat with doc api

--- src/index.ts
+++ src/index.ts
@@ -39,3 +39,29 @@ app.post('/translateDoc', async (c)=>{
 })
 
 export default app;
+
+app.post('/chatWithDoc', async (c)=>{
+	const openai = new OpenAI({
+		apiKey: c.env.OPEN_AI_KEY,
+	})
+
+	const { docData, question } = await c.req.json();
+
+	const chatCompletion = await openai.chat.completions.create({
+		messages:[
+			{
+				role:	'system',
+				content: 'You are a assistant helping user to chat with a document, I am providing a JSON file of the markdown for the document. Using this, answer the users question in the clearest way possible. Do not answer any question which is not related to the document. The document markdown JSON is: ' + docData,
+			}, {
+				role: 'user',
+				content: 'User question is: ' + question,
+			},
+		],
+		model: 'gpt-4o-mini',
+		temperature: 0.5,
+	})
+
+	const response = chatCompletion.choices[0].message.content;
+
+	return c.json({ message: response });
+})
